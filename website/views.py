42d3b81beed763a9cef21b3b7ba6ddeb22057a55
Austin Ward <austsethw@hotmail.com>
2023-04-05 21:24:00 -0400

    Setting up initail urls, views, and templates.

--- website/views.py
+++ website/views.py
@@ -1,4 +1,44 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
+from .forms import SignUpForm, AddRecordForm
 
 def home(request):
-    return render(request, 'home.html', {})
+    # Check to see if logging in
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        # Authenticate
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            messages.success(request, "You have been logged in!")
+            return redirect('home')
+        else:
+            messages.success(request, "There was an error logging in. Please try again...")
+            return redirect('home')
+    else:
+        return render(request, 'home.html', {''})
+
+def logout_user(request):
+    logout(request)
+    messages.success(request, "You have been logged out...")
+    return redirect('home')
+
+def register_user(request):
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            # Authenticate and login
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password1']
+            user = authenticate(username=username, password=password)
+            login(request, user)
+            messages.success(request, "You have Successfully Registered! Welcome!")
+            return redirect('home')
+    else:
+        form = SignUpForm()
+        return render(request, 'register.html', {'form':form})
+    
+    return render(request, 'register.html', {'form':form})
